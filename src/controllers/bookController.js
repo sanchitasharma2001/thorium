bcf90878e96921cbacc8cd353f29d9f5216e9e14
Sanchita Sharma <sanchitasharma2001@gmail.com>
2022-03-02 04:18:19 +0530

    Done

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,59 +1,62 @@
 const { count } = require("console")
 const BookModel= require("../models/bookModel")
-
-const createBook= async function (req, res) {
-    let data= req.body
-
-    let savedData= await BookModel.create(data)
-    res.send({msg: savedData})
+const authorModel= require("../models/authorModel")
+//part 1
+const createBook=async function (req, res){
+    let data= req .body
+    let saveddata=await BookModel.create(data)
+    res.send({msg:saveddata})
 }
 
-const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find( {authorName : "HO" } )
-    console.log(allBooks)
-    if (allBooks.length > 0 )  res.send({msg: allBooks, condition: true})
-    else res.send({msg: "No books found" , condition: false})
+//part 2
+const createAuthor=async function (req, res){
+    let data= req .body
+    let saveddata=await authorModel.create(data)
+    res.send({msg:saveddata})
+
 }
 
-
-const updateBooks= async function (req, res) {
-    let data = req.body // {sales: "1200"}
-    // let allBooks= await BookModel.updateMany( 
-    //     { author: "SK"} , //condition
-    //     { $set: data } //update in data
-    //  )
-    let allBooks= await BookModel.findOneAndUpdate( 
-        { authorName: "ABC"} , //condition
-        { $set: data }, //update in data
-        { new: true , upsert: true} ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT  
-     )
-     
-     res.send( { msg: allBooks})
+//part 3
+const bookList=async function (req, res){
+    let idAuthor= await authorModel.find({authorName:"Chetan Bhagat"}).select({author_id:1,_id:0})
+    console.log(idAuthor)
+    const id=idAuthor[0].author_id
+    console.log(id)
+    let bookNames=await BookModel.find({author_id:id}).select({name:1,_id:0})
+    console.log(bookNames)
+    console.log({msg:bookNames})
 }
 
-const deleteBooks= async function (req, res) {
-    // let data = req.body 
-    let allBooks= await BookModel.updateMany( 
-        { authorName: "FI"} , //condition
-        { $set: {isDeleted: true} }, //update in data
-        { new: true } ,
-     )
-     
-     res.send( { msg: allBooks})
+//4
+const updateBookPrice=async function(req, res){
+let idBook = await BookModel.findAndUpdate({name:"Two states"},{$set:{price:100}},{new:true}).select({author_id:1,_id:0})
+console.log(idBook)
+let id =idBook.author_id
+let newPrice=idBook.price
+console.log(newPrice)
+console.log(id)
+let authorName=await authorModel.find({author_id:id}).select({author_id:1,_id:0})
+console.log(authorName)
+res.send({msg:authorName,newPrice})
 }
-
-
-
-
-// CRUD OPERATIONS:
-// CREATE
-// READ
-// UPDATE
-// DELETE
-
-
-
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.updateBooks= updateBooks
-module.exports.deleteBooks= deleteBooks
+//part 5
+const Books=async function(req,res)
+{
+let avaliableBooks=await bookModel.find({price:{$gte:50,$lte:100}}).select({author_id:1,_id:0}) 
+console.log(avaliableBooks)   
+let id =availableBooks.map(ele=>ele.author_id)
+console.log(id)
+let arr=[]
+for(let i=0;i<id.length;i++){
+let x=id[i]
+let authorNames=await authorModel.find({author_id:x}).select({authorName:1,_id:0})
+arr.push(authorNames)
+}
+console.log(arr)
+res.send({msg:arr})
+}
+module.exports .createBook= createBook
+module.exports.createAuthor= createAuthor
+module.exports .bookList= bookList
+module.exports.updateBookPrice=updateBookPrice
+module.exports.Books= Books
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,25 +1,22 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
+    name: String, 
+    author_id: {
+        require: true,
+         type: String
+        } ,
+    price: Number,
+    rating: Number
+
     
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
-    },
-    sales: {type: Number, default: 10},
-    
-    // " best boook on earth"   [ "Nodejs in detail" , "mongodb in detail", "fronend in detail"] 
-    // {
-        // "ch1 ": "awesome intro to JS",
-        // "ch2" : "intro to nodejs",
-        // "ch3" : "intro to db"
-    //  }
-    summary :  mongoose.Schema.Types.Mixed,
-    isDeleted: Boolean //true on book deletion i.e you flag the document/data as isDeleted: true..(mark "dirty")
+     // " best boook on earth"   [ "Nodejs in detail" , "mongodb in detail", "fronend in detail"] 
+   // {
+  // "ch1 ": "awesome intro to JS",
+  // "ch2" : "intro to nodejs",
+  // "ch3" : "intro to db"
+ //  }
+ //true on book deletion i.e you flag the document/data as isDeleted: true..(mark "dirty")
 
 }, { timestamps: true });
 
